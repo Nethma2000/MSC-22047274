36ffe5779ba9baf05a07eccb98f937fe9141856d
Nethma Dhananjani <64515990+Nethma2000@users.noreply.github.com>
2024-08-01 10:09:19 +0530

    Delete admins directory

--- admins/blog/admin/home.php
+++
@@ -1,5 +0,0 @@
-<div class="container-fluid">
-	<div class="col-md-12 alert alert-info">
-		Dashboard
-	</div>
-</div>
--- admins/inqconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//for MySQLi OOP
-	$conn = new mysqli('localhost', 'root', '', 'startupcompanion');
-	if($conn->connect_error){
-	   die("Connection failed: " . $conn->connect_error);
-	}
-
-?>
